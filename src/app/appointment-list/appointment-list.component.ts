824ddf27ff10c0bb6c7666b1c69b2b9942e5f788
Behnam <behnam.vosoogh@outlook.com>
2025-04-06 18:34:35 -0400

    creating two ways data binding

--- src/app/appointment-list/appointment-list.component.ts
+++ src/app/appointment-list/appointment-list.component.ts
@@ -7,10 +7,8 @@ import { Appointment } from '../models/appointment';
   styleUrls: ['./appointment-list.component.css']
 })
 export class AppointmentListComponent {
-  appointment: Appointment = {
-    id:1,
-    title:"Take dog for a walk",
-    date: new Date('2025-07-06'),
-  };
+  newAppointemntTitle: string = '';
+  newAppointemntDate: Date = new Date();
+  appointments: Appointment []= [];
 
 }
